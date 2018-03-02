5a59036acefd79c5d895b2728df3b396f46aefa3
张雷雷 <zhangleilei@wecash.net>
2018-03-02 22:48:15 +0800

    a

--- client/app.js
+++ client/app.js
@@ -5,5 +5,26 @@ var config = require('./config')
 App({
     onLaunch: function () {
         qcloud.setLoginUrl(config.service.loginUrl)
-    }
+    },
+    // router
+    router: function(url, params) {
+        wx.navigateTo({
+            url: this.getUrls(url, params),
+        });
+    },
+    redirect: function(url, params) {
+        wx.redirectTo({
+            url: this.getUrls(url, params),
+        });
+    },
+    routerTab: function() {
+        wx.switchTab({
+            url: '../home/home'
+        });
+    },
+    getUrls: function(url, params) {
+        let urls = ((util.isEmpty(url)) ? '../temp/temp' : url) + (util.isEmpty(params) ? '' : '?params=' + JSON.stringify(params));
+        console.log('getUrls: ' + urls);
+        return urls;
+    },
 })
